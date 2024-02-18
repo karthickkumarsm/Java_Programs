8cea411f69e2f425b458ea89f23ad25d7903a4f0
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-02-18 20:49:59 +0530

    java day6

---
+++ DS Programs/day6.java
@@ -0,0 +1,22 @@
+/**
+ * day6
+ */
+public class day6 {
+    public static void main(String[] args) {
+        String s1="cat";
+        String s2="act";
+        for(int i=0;i<s1.length();i++){
+            for(int j=0;j<s2.length();j++){
+                if (s1.charAt(i)==s2.charAt(j)) {
+                    s2=s2.replaceFirst(""+s1.charAt(i), "");
+                    break;
+                }
+            }
+        }
+        if(s2.length()==0){
+            System.out.println("Given word is anagram");
+        }else{
+            System.out.println("Given word is not an anagram");
+        }
+    }
+}
