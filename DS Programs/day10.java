012bafcd408c2623daf92c41b29b2ae51f0eddda
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-02-23 17:22:27 +0530

    java

---
+++ DS Programs/day10.java
@@ -0,0 +1,29 @@
+public class day10 {
+    public static void main(String[] args) {
+        String s="  Hardwork never fails    ";
+        int f=0,l=s.length()-1;
+        boolean first=false,last=true;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)==' ' && first==false){
+                f++;
+            }
+            else{
+                first=true;
+            }
+            if(s.charAt(l)==' ' && last==false){
+                l--;
+            }
+            else{
+                last=true;
+            }
+            if(first==true && last==true){
+                break;
+            }
+        }
+        for(int i=f;i<=l;i++){
+            System.out.print(s.charAt(i));
+        }
+    }
+}
+
+//trim() method logic
