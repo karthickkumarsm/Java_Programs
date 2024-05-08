27b6aa446714f114d7c045e4c10bc3a0558f2011
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-05-08 22:57:57 +0530

    day 17

--- DS Programs/day17.java
+++ DS Programs/day17.java
@@ -2,15 +2,18 @@ public class day17 {
     public static void main(String[] args) {
         day17 pd=new day17();
         char a='{',b='}';
-        for(int j=1;j<=1;j++){
+        int count=3;
+        for (int i = 1; i <= count; i++) {
+        for(int j=1;j<=i;j++){
             for(int k=1;k<=j;k++){
-                System.out.println(a);
+                System.out.print(a);
             }
             for(int k=1;k<=j;k++){
-                System.out.println(b);
-                System.out.print(" ");
+                System.out.print(b);
+                System.out.print("");
             }
             System.out.println();
         }
+    }
     }
 }
