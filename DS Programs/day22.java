4a215ee4cf388f50207828bb6d18f71d13b12e73
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-08-21 18:36:28 +0530

    java practice

---
+++ DS Programs/day22.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class day22 {
+    public static void main(String[] args) {
+        int amount=8368;
+        currency(amount);
+    }
+    public static void currency(int amount){
+        int notes[] = new int[]{500,200,100,50,20,10,5,2,1};
+        int notecount[] = new int[9];
+        for(int i=0;i<9;i++){
+            if(amount>=notes[i]){
+                notecount[i] = amount / notes[i];
+                amount = amount % notes[i];
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            if (notecount[i] != 0) {
+                System.out.println(notes[i] + " : "
+                    + notecount[i]);
+            }
+        }
+    }
+}
