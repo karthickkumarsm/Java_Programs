fc22c38fccb23f04c4dcc142dba6c0d19bf7344c
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-09-07 15:15:52 +0530

    day 28

---
+++ DS Programs/day27.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class day27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String s = sc.nextLine();
+        char[] ch = s.toCharArray();
+        int count=0;
+        char c = sc.next().charAt(0);
+        for(int i=0;i<n;i++){
+            if(ch[i]==c){
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
+
+//accenture problem 1 : Character count
---
+++ DS Programs/day28.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class day28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int v = sc.nextInt();
+        int vn = sc.nextInt();
+        int e = v/vn;
+        int en = (int)Math.pow(e, 2);
+        System.out.println(h*en);
+    }
+}
