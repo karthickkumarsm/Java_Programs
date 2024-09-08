d9de2fccd1b96bc1c20dc48a8eb20b514ae48fcb
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-09-08 11:41:57 +0530

    day 33

---
+++ DS Programs/day34.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day34 {
+    public static void main(String[] args){
+        //write your code here
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+
+        System.out.println(fibo(n));
+    }
+    public static int fibo(int n){
+        if(n <= 1)return 1;
+        return (fibo(n-1)* fibo(n-1) + fibo(n-2)*fibo(n-2)) % 47;
+    }
+}
